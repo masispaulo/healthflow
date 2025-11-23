6243006b8d4cfc02511ed72bc000d6e27316360e
masispaulo <pauloverissimodemazzi@gmail.com>
2025-11-23 08:13:20 -0400

    Refactor Nginx config handling in run.sh

--- run.sh
+++ run.sh
@@ -3,15 +3,11 @@
 # Define a porta padrão se a variável de ambiente PORT não estiver definida.
 export PORT=${PORT:-8080}
 
-# O Nginx precisa de um arquivo de configuração temporário para o envsubst funcionar.
-TEMPLATE_FILE=/etc/nginx/conf.d/default.conf.template
-CONFIG_FILE=/etc/nginx/conf.d/default.conf
-
 # Move o arquivo de configuração original para servir como template.
-mv $CONFIG_FILE $TEMPLATE_FILE
+mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf.template
 
 # Substitui a variável ${PORT} no template e salva o resultado no arquivo de configuração final.
-envsubst '$PORT' < $TEMPLATE_FILE > $CONFIG_FILE
+envsubst '$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 # Inicia o Nginx em primeiro plano.
 nginx -g "daemon off;"
